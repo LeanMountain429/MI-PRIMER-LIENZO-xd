4b2cef5d942d3046883366045157cf70a0a98c63
LeanMountain429 <115753645+LeanMountain429@users.noreply.github.com>
2022-12-27 18:58:09 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var T_T= document.getElementById("Agh_xd")
+var U_U= T_T.getContext("2d")
+var X_X= "blue"
+var UwU= 2
+var A_A= "vachio"
+T_T.addEventListener("mousedown",IhateMITappINVENTOR)
+function IhateMITappINVENTOR(ñ){
+    A_A= "mousedown"
+}
+T_T.addEventListener("mousemove",IloveSTUDIOcode)
+function IloveSTUDIOcode(ñ){
+    var Y_Y= ñ.clientY-T_T.offsetTop
+    var OwO= ñ.clientX-T_T.offsetLeft
+    if (A_A=="mousedown"){
+        U_U.beginPath()
+        U_U.strokeStyle=X_X
+        U_U.lineWidth=UwU
+        U_U.moveTo(N_N,QwQ)
+        U_U.lineTo(OwO,Y_Y)
+        U_U.stroke()
+    }
+    N_N=OwO
+    QwQ=Y_Y
+}
+T_T.addEventListener("mousedown",IhateMITappINVENTOR)
+function IhateMITappINVENTOR(ñ){
+    A_A= "mousedown"
+}
